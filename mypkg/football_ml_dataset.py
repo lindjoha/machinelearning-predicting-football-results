0f19dcb88e709da1287a7a09f55e3429e273950a
Øyvind Lind-Johansen <OLIND@statoil.com>
2019-04-21 20:58:20 +0200

    Implemented SVM, and some changes to the feature engineering

--- mypkg/football_ml_dataset.py
+++ mypkg/football_ml_dataset.py
@@ -50,14 +50,16 @@ class MatchStatsDataset:
         return df
 
 
-    def get_subset(self, team_name, home_or_away, before_date, n_games):
+    def get_subset(self, team_name, home_away_both, before_date, n_games):
         filtered = self.matchstats[self.matchstats.Date < before_date]
-        if home_or_away == 'H':
+        if home_away_both == 'home':
             filtered = filtered[filtered.HomeTeam == team_name] 
-        elif home_or_away == 'A':
+        elif home_away_both == 'away':
             filtered = filtered[filtered.AwayTeam == team_name]
+        elif home_away_both == 'both':
+            filtered = filtered[(filtered.HomeTeam == team_name) | (filtered.AwayTeam == team_name)]
         else:
-            print('Parameter ''home_or_away'' must be ''H'' or ''A''')
+            raise ValueError('Variable ''home_away_both'' must be ''home'', ''away'' or ''both''.')
         return filtered.tail(n_games)
 
 
@@ -86,48 +88,59 @@ class Match:
 
 class Feature():
     
-    def __init__(self, ha_team, feat_type, n_games, ha_games):
+    def __init__(self, ha_team, feat_type, n_games, home_away_both_games):
         self.ha_team = ha_team
-        self.ha_games = ha_games
+        self.home_away_both_games = home_away_both_games
         self.feat_type = feat_type
         self.n_games = n_games
         
     def get_value(self, msd, match):
         team_name = match.get_home_or_away_team(self.ha_team)
-        df = msd.get_subset(team_name, self.ha_games, match.date, self.n_games)
+        df = msd.get_subset(team_name, self.home_away_both_games, match.date, self.n_games)
         if df.shape[0] == 0:
             return math.nan
         
+        df_home_games = df[df.HomeTeam == team_name]
+        df_away_games = df[df.AwayTeam == team_name]
+        
         if self.feat_type == 'prcwin':
-            result_count = df['FTR'].value_counts()
-            if self.ha_games in result_count.index:
-                return result_count.loc[self.ha_games]/df.shape[0]
-            else:
-                return 0
+            nb_home_wins = self.get_number_of_wins(df_home_games, 'H')
+            nb_away_wins = self.get_number_of_wins(df_away_games, 'A')
+            return (nb_home_wins + nb_away_wins)/df.shape[0]
         
         if self.feat_type == 'goalsscored':
-            column = 'FT'+self.ha_games+'G'
-            return df[column].sum()
+            return df_home_games['FTHG'].sum() + df_away_games['FTAG'].sum()
         
         if self.feat_type == 'goalsagainst':
-            column='FTAG' if self.ha_games == 'H' else 'FTHG'
-            return df[column].sum()
+            return df_home_games['FTAG'].sum() + df_away_games['FTHG'].sum()
         
         if self.feat_type == 'goalsscoredewma':
-            column = 'FT'+self.ha_games+'G'
-            return df[column].ewm(com=0.25).mean().iloc[-1]
+            return self.calc_goalsscored_ewma(df, team_name, 0.2)
             
         else:
             return math.nan
             print('Feature type: ' + self.feat_type + ' is not implemented')
-        
-
-#%% Testcode
-#path ='C:/Users/olind/ML/'
-#df_PL0506 = pd.read_csv(path+'PL0506.csv')
-#ds_PL0506 = MatchStatsDataset(df_PL0506, 300)
-#dct_features = {'ht_prcwin_prev5_homegames':Feature('H', 'prcwin', 5, 'H'),
-#            'ht_goalsscoredewma_prev20_homegames':Feature('H', 'goalsscoredewma', 20, 'H'),
+          
+    def get_number_of_wins(self, df, result):
+        result_count = df['FTR'].value_counts()
+        if result in result_count.index:
+            return result_count[result]
+        else:
+            return 0
+    
+    def calc_goalsscored_ewma(self, df, team_name, com):
+        ewma = 0
+        for i, row in df.iterrows():
+            column = 'FTHG' if row.HomeTeam == team_name else 'FTAG'
+            ewma += row[column]*com
+        return ewma
+            
+#
+#path ='C:/Users/olind/machinelearning-predicting-football-results/Data/'
+#df = pd.read_csv(path+'CH1718.csv')
+#ds = MatchStatsDataset(df, 300)
+#dct_features = {
+#            'ht_goalsscoredewma_prev20_games':Feature('H', 'goalsscoredewma', 10, 'both'),
 #            }
-#features = ds_PL0506.get_features_and_matchdata(dct_features)
-#print(features[list(dct_features.keys())])
+#features = ds.get_features_and_matchdata(dct_features)
+
